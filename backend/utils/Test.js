a532a628913602a56c9a3c6747f40178522dcf41
CodeBuddySLTNS <renz2arjhay@gmail.com>
2025-02-18 06:15:19 +0800

    added utilities and sample data

---
+++ backend/utils/Test.js
@@ -0,0 +1,162 @@
+console.log(JSON.stringify({
+  success: true,
+  message: "",
+  response: [
+    {
+      _id: "6799caa3ead756404411e03a",
+      message: "Test",
+      date: "2025-01-29T00:00:00.000Z",
+      likes: 0,
+      whoLiked: [],
+      author: {
+        _id: "6799ca8fead756404411e023",
+        id: 69,
+        name: "Ace",
+        img: "",
+        username: "Itzmeac3",
+        password:
+          "$2a$10$JLuZWWhdAEPlq2JE6rgEfegSyUcikSJiaPMY8O7J1AANq0PEBaLLa",
+        role: "Member",
+        liked: [],
+        createdAt: "2025-01-29T06:28:31.263Z",
+        updatedAt: "2025-01-29T06:28:31.263Z",
+        __v: 0
+      },
+      comments: [],
+      createdAt: "2025-01-29T06:28:51.572Z",
+      updatedAt: "2025-01-29T06:28:51.572Z",
+      __v: 0
+    },
+    {
+      _id: "6767c998b7701a53c4ee3b33",
+      message: "shesh",
+      date: "2024-12-22T00:00:00.000Z",
+      likes: 1,
+      whoLiked: ["673051801df1114b6cd4bd72"],
+      author: {
+        _id: "673051801df1114b6cd4bd72",
+        id: 52,
+        name: "Jerome",
+        img: "",
+        username: "@jerome123",
+        password:
+          "$2a$10$FLj2zNy04f2yna32a3TpqOY.ofPEwSuh32D9BC6mBeiKB4BhzHDbi",
+        role: "Member",
+        liked: [],
+        createdAt: "2024-11-10T06:24:00.649Z",
+        updatedAt: "2024-11-10T06:24:00.649Z",
+        __v: 0
+      },
+      comments: [],
+      createdAt: "2024-12-22T08:11:04.199Z",
+      updatedAt: "2025-01-29T06:28:59.949Z",
+      __v: 0
+    },
+    {
+      _id: "6763a222cfb4f537532bd7a7",
+      message: "Buhay paba kau?",
+      date: "2024-12-19T00:00:00.000Z",
+      likes: 3,
+      whoLiked: [
+        "672f34391df1114b6cd4b845",
+        "673051801df1114b6cd4bd72",
+        "67998963ead756404411df45"
+      ],
+      author: {
+        _id: "672f34391df1114b6cd4b845",
+        id: 25,
+        name: "joshuaApostol",
+        img: "https://i.imgur.com/ZYQyBn5.jpeg",
+        username: "joshuaapostol",
+        password:
+          "$2a$10$N7IvKtevYrMmif/mBcrRJuQt2T0a2L/6/p.Fi.JOZASfRWG7jNNmS",
+        role: "Admin",
+        liked: [],
+        createdAt: "2024-11-09T10:06:49.739Z",
+        updatedAt: "2024-11-09T10:52:41.040Z",
+        __v: 0
+      },
+      comments: [],
+      createdAt: "2024-12-19T04:33:38.014Z",
+      updatedAt: "2025-01-29T01:50:52.188Z",
+      __v: 0
+    },
+    {
+      _id: "67455ceac48ef52e05b09edf",
+      message: "Bossing kamusta ang buhay buhay?",
+      date: "2024-11-26T00:00:00.000Z",
+      likes: 1,
+      whoLiked: ["6725c303357370b452dbd4bf"],
+      author: {
+        _id: "672f34391df1114b6cd4b845",
+        id: 25,
+        name: "joshuaApostol",
+        img: "https://i.imgur.com/ZYQyBn5.jpeg",
+        username: "joshuaapostol",
+        password:
+          "$2a$10$N7IvKtevYrMmif/mBcrRJuQt2T0a2L/6/p.Fi.JOZASfRWG7jNNmS",
+        role: "Admin",
+        liked: [],
+        createdAt: "2024-11-09T10:06:49.739Z",
+        updatedAt: "2024-11-09T10:52:41.040Z",
+        __v: 0
+      },
+      comments: [
+        {
+          message: "Katamad na e hahahah",
+          date: "11/26/2024",
+          whoLiked: [],
+          author: {
+            _id: "6725c303357370b452dbd4bf",
+            id: 2,
+            name: "Juan Tamad",
+            img: "https://i.imgur.com/shsLyFB.jpeg",
+            username: "Juantamad",
+            password:
+              "$2a$10$dh2WNqhNFz6gDvYu77lZMezq/b0gre1i0O82ht68t4i.PtftKoyYO",
+            role: "Admin",
+            liked: [],
+            createdAt: "2024-11-02T06:13:23.733Z",
+            updatedAt: "2024-11-05T12:24:15.668Z",
+            __v: 0
+          },
+          _id: "6745cfb5c48ef52e05b09f90",
+          createdAt: "2024-11-26T13:40:05.160Z",
+          updatedAt: "2024-11-26T13:40:05.160Z",
+          replies: []
+        }
+      ],
+      createdAt: "2024-11-26T05:30:18.802Z",
+      updatedAt: "2024-11-26T13:40:05.160Z",
+      __v: 0
+    },
+    {
+      _id: "673df7ffc48ef52e05b09d5b",
+      message: "Wala po update?",
+      date: "2024-11-20T00:00:00.000Z",
+      likes: 2,
+      whoLiked: ["673ca227c48ef52e05b09ada", "6725c303357370b452dbd4bf"],
+      author: {
+        _id: "673ca227c48ef52e05b09ada",
+        id: 65,
+        name: "Rex Dev",
+        img: "",
+        username: "Rex Dev",
+        password:
+          "$2a$10$rzIePCA..Uv1EV7LjCTKmuqjKCVYTL2LATxFW2IKJ1UYh.NUaB7Ma",
+        role: "Member",
+        liked: [],
+        createdAt: "2024-11-19T14:35:19.459Z",
+        updatedAt: "2024-11-19T14:35:19.459Z",
+        __v: 0
+      },
+      comments: [],
+      createdAt: "2024-11-20T14:53:51.769Z",
+      updatedAt: "2024-11-26T13:40:25.988Z",
+      __v: 0
+    }
+  ],
+  errors: null,
+  dev: "Juan Tamad",
+  devFb: "@thejuantamad05"
+}))
